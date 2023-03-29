ae03bd4ebe8f9b03561345fec235fe92778de19a
ljaviertovar <luisjavier_tovar@outlook.com>
2023-03-29 13:41:35 -0400

    update scrape

--- scrapers/screen-botsito.js
+++ scrapers/screen-botsito.js
@@ -3,7 +3,6 @@ require("dotenv").config()
 
 const screenBotsito = async (res) => {
   const browser = await puppeteer.launch({
-    ignoreDefaultArgs: true,
     args: [
       "--disable-setuid-sandbox",
       "--no-sandbox",
@@ -14,21 +13,36 @@ const screenBotsito = async (res) => {
       process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
-    // headless: process.env.NODE_ENV === "production" ? true : false
   })
   try {
     const page = await browser.newPage()
 
     await page.goto("https://developer.chrome.com/")
 
-    await page.screenshot({ path: 'jobs.jpg', fullPage: true })
-    console.log("--BOTSITO SCREEN--")
+    // Set screen size
+    await page.setViewport({ width: 1080, height: 1024 })
 
-    res.status(200).json({ "screen": "" })
+    // Type into search box
+    await page.type(".search-box__input", "automate beyond recorder")
+
+    // Wait and click on first result
+    const searchResultSelector = ".search-box__link"
+    await page.waitForSelector(searchResultSelector)
+    await page.click(searchResultSelector)
+
+    // Locate the full title with a unique string
+    const textSelector = await page.waitForSelector(
+      "text/Customize and automate"
+    )
+    const fullTitle = await textSelector.evaluate((el) => el.textContent)
+
+    // Print the full title
+    const logStatement = `The title of this blog post is ${fullTitle}`
+    console.log(logStatement)
+    res.send(logStatement)
   } catch (e) {
-    console.log(e)
-    res.status(200).json({ "error": e })
-
+    console.error(e)
+    res.send(`Something went wrong while running Puppeteer: ${e}`)
   } finally {
     await browser.close()
   }
