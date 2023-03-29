4ac8c4021e06d225410761d4dab3a73d06f4872b
ljaviertovar <luisjavier_tovar@outlook.com>
2023-03-29 13:51:03 -0400

    update scrape

--- scrapers/screen-botsito.js
+++ scrapers/screen-botsito.js
@@ -17,24 +17,13 @@ const screenBotsito = async (res) => {
   try {
     const page = await browser.newPage()
 
-    await page.goto("https://developer.chrome.com/")
+    await page.goto('https://hiring.amazon.ca/app#/jobSearch')
 
-    // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 })
+    const cookieBtn = ".search-box__link"
+    await page.waitForSelector(cookieBtn)
+    await page.click(cookieBtn)
 
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder")
 
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link"
-    await page.waitForSelector(searchResultSelector)
-    await page.click(searchResultSelector)
-
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    )
-    const fullTitle = await textSelector.evaluate((el) => el.textContent)
 
     // Print the full title
     const logStatement = `The title of this blog post is ${fullTitle}`
