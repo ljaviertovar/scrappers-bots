2add2b054aa8a4206916eb3655b58d472c838fa6
ljaviertovar <luisjavier_tovar@outlook.com>
2023-03-29 15:13:43 -0400

    update scrape

--- scrapers/screen-botsito.js
+++ scrapers/screen-botsito.js
@@ -20,7 +20,7 @@ const screenBotsito = async (res) => {
     console.log("GOTO")
 
     await page.goto('https://hiring.amazon.ca/app#/jobSearch', { waitUntil: 'load', timeout: 0 })
-    await new Promise(r => setTimeout(r, 1000))
+    // await new Promise(r => setTimeout(r, 1000))
 
     console.log("IN PAGE")
     const cookieBtnEl = "#stencil-modal-body > div > div > div > div > div:nth-child(2) > button"
